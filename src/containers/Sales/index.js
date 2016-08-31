9653aa5412095323b76c5ffca923ac38e62c1071
Kristoffer K Larsen <kristoffer@larsen.so>
2016-09-01 00:27:14 +0200

    Fetch user based on lookupParam

--- src/containers/Sales/index.js
+++ src/containers/Sales/index.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 import TabMenu from '../../components/TabMenu';
 import ProductContainer from '../../components/ProductContainer';
+import SearchModal from '../../components/SearchModal';
 import Product from '../../components/Product';
 import Sidebar from '../../components/Sidebar';
 import Style from './Sales.css';
 
 class SalesContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: true
+    };
+  }
+
   render() {
     return (
       <div className={Style.salesContainer}>
-        <div className={Style.main}>
+        <SearchModal
+          active={this.state.search}
+          onDismiss={() => { this.setState({ search: false }); }}
+          onSuccess={() => { this.setState({ search: false }); }}
+        />
+        <div className={classNames(Style.blur, { [Style.active]: this.state.search })} >
+          <div className={Style.main}>
 
-          <TabMenu />
-          <ProductContainer>
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-            <Product name='Fanta' price='59' />
-          </ProductContainer>
+            <TabMenu />
+            <ProductContainer>
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+              <Product name='Fanta' price='59' />
+            </ProductContainer>
 
+          </div>
+          <Sidebar />
         </div>
-        <Sidebar />
       </div>
     );
   }
