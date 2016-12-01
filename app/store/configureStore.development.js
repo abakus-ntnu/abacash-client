d752a6e3dc039adbfde453a1810ee3f316dc3f70
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2016-12-01 14:41:56 +0100

    Persist store

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { hashHistory } from 'react-router';
 import { routerMiddleware, push } from 'react-router-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import immutableTransform from 'redux-persist-transform-immutable';
 import createLogger from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from '../reducers';
 
+const PERSISTENCE_ENABLED = process.env.PERSISTENCE_ENABLED;
 
 const actionCreators = {
   push,
@@ -28,6 +31,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
   compose;
 /* eslint-enable no-underscore-dangle */
 const enhancer = composeEnhancers(
+  PERSISTENCE_ENABLED ? autoRehydrate() : (f) => f,
   applyMiddleware(
     thunk,
     router,
@@ -36,8 +40,17 @@ const enhancer = composeEnhancers(
   )
 );
 
-export default function configureStore(initialState: Object) {
+export default function configureStore(initialState: Object, onComplete: ?() => void) {
   const store = createStore(rootReducer, initialState, enhancer);
+
+  if (PERSISTENCE_ENABLED) {
+    persistStore(store, {
+      transforms: [immutableTransform({})],
+      whitelist: ['auth'],
+    }, onComplete);
+  } else {
+    setTimeout(onComplete, 50);
+  }
 
   if (module.hot) {
     module.hot.accept('../reducers', () =>
