fbf34c0263cf44065414115d06296b877e1c554b
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-02-07 20:43:23 +0100

    Support the new product group API

--- app/reducers/product.js
+++ app/reducers/product.js
@@ -4,11 +4,18 @@ import { PRODUCT } from '../actions/types';
 import type { Reducer } from './types';
 
 const initialState = fromJS({
-  products: {}
+  groups: [],
+  products: {},
 });
 
 const product: Reducer = (state = initialState, action) => {
   switch (action.type) {
+
+    case PRODUCT.FETCH_GROUPS_SUCCESS: {
+      return state.merge({
+        groups: action.payload.json.entities['product-group']
+      });
+    }
 
     case PRODUCT.FETCH_PRODUCTS_SUCCESS: {
       return state.merge({
