f8bff4155ee4b85b625963a119265794f3073bfe
larseen <kristoffer@larsen.so>
2017-03-15 13:50:08 +0100

    Fix bug with not having seller

--- app/actions/transaction.js
+++ app/actions/transaction.js
@@ -10,12 +10,17 @@ export function createTransaction(): Thunk {
     const customer = getState().customer.get('customer');
     const seller = getState().system.get('seller');
     const cart = getState().cart;
+    let sellerId = null;
+
+    if (seller) {
+      sellerId = seller.get('id');
+    }
 
     return dispatch(callAPI({
       type: TRANSACTION.CREATE_TRANSACTION,
       body: {
         products: mapCartToTransaction(cart),
-        sellerId: seller.get('id'),
+        sellerId,
         customerId: customer.get('id')
       },
       method: 'POST',
